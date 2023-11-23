3e8dbe80096d9dc374f690670f6fe9b824254d2b
Colby Robins <colbyr2891@gmail.com>
2023-11-22 19:07:50 -0700

    Add model associations.

--- models/index.js
+++ models/index.js
@@ -8,6 +8,15 @@ User.hasMany(BlogPost, {
     foreignKey: 'creator_id'
 });
 
+BlogPost.belongsTo(User, {
+    foreignKey: 'creator_id'
+});
+
+User.hasMany(Comment, { foreignKey: 'user_id' });
+Comment.belongsTo(User, { foreignKey: 'user_id' });
+
+BlogPost.hasMany(Comment, { foreignKey: 'blog_post_id' });
+Comment.belongsTo(BlogPost, { foreignKey: 'blog_post_id' });
 
 
 
