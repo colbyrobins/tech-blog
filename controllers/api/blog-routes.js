8ac57bdefbd79bb72db6c5ac4e9ec3eeceb1a03c
Colby Robins <colbyr2891@gmail.com>
2023-11-24 09:23:17 -0700

    Added HTML to main.handlebars with styling.

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,30 +1,6 @@
 const router = require("express").Router();
 const BlogPost = require("../../models/BlogPost");
 
-// route to get all blogs
-router.get("/", async (req, res) => {
-    try {
-        const blogData = await BlogPost.findAll();
-        res.status(200).json(blogData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// route to get one blog
-router.get("/:id", async (req, res) => {
-    try {
-        const blogData = await BlogPost.findByPk(req.params.id);
-        if (!blogData) {
-            res.status(404).json({ message: "No blog found with this id!" });
-            return;
-        }
-        res.status(200).json(blogData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 // route to create a blog
 router.post("/", async (req, res) => {
     try {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,4 +11,18 @@ router.get("/", async (req, res) => {
     }
 });
 
+// // route to get one blog
+// router.get("/:id", async (req, res) => {
+//     try {
+//         const blogData = await BlogPost.findByPk(req.params.id);
+//         if (!blogData) {
+//             res.status(404).json({ message: "No blog found with this id!" });
+//             return;
+//         }
+//         res.status(200).json(blogData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
 module.exports = router;
