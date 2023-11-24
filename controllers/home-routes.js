907944c27865d60def0456caa17cd9334678a6b7
Colby Robins <colbyr2891@gmail.com>
2023-11-24 10:00:21 -0700

    Fixed some formatting on the home page and added author and date for each blog

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
+const { User } = require("../models");
 const BlogPost = require("../models/BlogPost");
 
 router.get("/", async (req, res) => {
     try {
-        const blogData = await BlogPost.findAll();
+        const blogData = await BlogPost.findAll({
+            include: User,
+            attributes: {
+                exclude: ["password"],
+            },
+        });
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
+        console.log(blogs)
         res.render("homepage", { blogs });
     } catch (err) {
         res.status(500).json(err);
--- seeds/blogPostData.js
+++ seeds/blogPostData.js
@@ -5,43 +5,43 @@ const { BlogPost } = require('../models');
 const blogPostData = [
   {
     "title": "My First Blog!!",
-    "content": "lernantino",
+    "content": "This is my first blog post!",
     "date_created": "11/22/2022",
     "creator_id": 1
   },
   {
     "title": "My Second Blog!!",
-    "content": "lernantino",
+    "content": "This is my second blog post!",
     "date_created": "11/22/2022",
     "creator_id": 2
   },
   {
     "title": "Fifth Blog Post",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "content": "This is my fifth blog post!",
     "date_created": "11/23/2022",
     "creator_id": 1
   },
   {
     "title": "Sixth Blog Post",
-    "content": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    "content": "This is my sixth blog post!",
     "date_created": "11/23/2022",
     "creator_id": 2
   },
   {
     "title": "Seventh Blog Post",
-    "content": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
+    "content": "This is my seventh blog post!",
     "date_created": "11/23/2022",
     "creator_id": 3
   },
   {
     "title": "Eighth Blog Post",
-    "content": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    "content": "This is my eighth blog post!",
     "date_created": "11/23/2022",
     "creator_id": 1
   },
   {
     "title": "Ninth Blog Post",
-    "content": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.",
+    "content": "This is my ninth blog post!",
     "date_created": "11/23/2022",
     "creator_id": 2
   }
